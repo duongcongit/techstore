57f6795d3771d6a5c6f2d0b539de62e9e4c2de94
duongcongit <duongcongit@gmail.com>
2023-06-10 19:31:20 +0700

    create apis manage employee for admin and add api get admin, customer, employee info

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,6 +1,7 @@
 import { DateTime } from "luxon";
 
 import Admin from "../models/Admin.js";
+import Employee from "../models/Employee.js";
 import Customer from "../models/Customer.js";
 
 
@@ -13,6 +14,27 @@ class AdminController {
       .then(admins => {
         res.status(200).send(admins)
       })
+
+  }
+
+  // Get admin info by username
+  getAdminInfo = (req, res) => {
+    // Data from client
+    let username = req.params.adminUsername;
+    Admin.findOne({ username: username }, { password: 0, __v: 0, activeCode: 0, _id: 0 })
+      .then(acc => {
+        if (acc != null) { // Send data to client
+          return res.status(200).send(acc);
+        }
+        else { // Account not found
+          res.status(404).json({
+            Error: "Admin account not found!"
+          });
+        }
+      })
+
+
+
 
   }
 
@@ -133,7 +155,151 @@ class AdminController {
   }
 
 
-  // ========== MANAGE USER ==========
+  // ========== MANAGE EMPLOYEE ==========
+
+  // Get all employees
+  getAllEmployees = (req, res) => {
+    Employee.find({}, { password: 0, __v: 0, activeCode: 0, _id: 0 })
+      .then(employees => {
+        return res.status(200).send(employees)
+      })
+  }
+
+  // Get employee info by username
+  getEmployeeInfo = (req, res) => {
+    // Data from client
+    let username = req.params.employeeUsername;
+    Employee.findOne({ username: username }, { password: 0, __v: 0, activeCode: 0, _id: 0 })
+      .then(acc => {
+        if (acc != null) { // Send data to client
+          return res.status(200).send(acc);
+        }
+        else { // Account not found
+          res.status(404).json({
+            Error: "Employee account not found!"
+          });
+        }
+      })
+
+
+
+
+  }
+
+  // Add employee
+  addEmployee(req, res) {
+    // Get data from client
+    let employeeAccountInfo = req.body;
+    let username = employeeAccountInfo.username;
+    // Check account is exists or not
+    Employee.find({ username: username })
+      .then(acc => {
+        if (acc.length >= 1) { // Exists
+          res.status(409).json({
+            Error: "Employee account is existed!"
+          });
+        }
+        else { // Create new account
+          let employeeAcc = new Employee(employeeAccountInfo);
+          employeeAcc.save()
+            .then(() => res.status(201).json({
+              Result: "Add employee account successfully."
+            }))
+            .catch(error => { });
+        }
+      })
+
+  }
+
+  // Update employee
+  updateEmployee = (req, res) => {
+    // Data from client
+    let username = req.body.username;
+    // Check account is exists or not
+    Employee.find({ username: username })
+      .then(acc => {
+        if (acc.length >= 1) { // If exist, update account
+          Employee.findOneAndUpdate({ username: username }, req.body)
+            .then(() => {
+              res.status(200).json({
+                Result: "Update employee account successfully."
+              })
+            })
+        }
+        else { // Account not found
+          res.status(404).json({
+            Error: "Employee account not found!"
+          });
+        }
+      })
+
+
+
+
+  }
+
+  // Soft delete
+  softDeleteEmployee = (req, res) => {
+    let username = req.body.username; // Data from client
+    let currentTime = DateTime.utc().toISO(); // Current time
+    // Data for soft delete
+    let accSoftDelDetail = {
+      status: -1,
+      deleteAt: currentTime
+    }
+    // Check account is exist or not
+    Employee.findOne({ username: username })
+      .then(acc => {
+        if (acc) { // If exist
+          if (acc.status == -1) { // If account has been soft delete before
+            res.status(204).json({
+              Error: "Employee account has been deleted before."
+            })
+          }
+          else { // Or not, do soft delete
+            Employee.findOneAndUpdate({ username: username }, accSoftDelDetail)
+              .then(() => {
+                res.status(200).json({
+                  Result: "Soft delete employee account successfully."
+                })
+              })
+          }
+        }
+        else { // Account not found
+          res.status(404).json({
+            Error: "Employee account not found!"
+          });
+        }
+      })
+
+  }
+
+  // Delete
+  deleteEmployee = (req, res) => {
+    let username = req.body.username; // Data from client
+    // Check account is exist or not
+    Employee.find({ username: username })
+      .then(acc => { // If exist, delete account
+        if (acc.length >= 1) {
+          Employee.findOneAndRemove({ username: username })
+            .then(() => {
+              res.status(200).json({
+                Result: "Delete employee account successfully."
+              })
+            })
+        }
+        else { //  Account not found
+          res.status(404).json({
+            Error: "Employee account not found!"
+          });
+        }
+      })
+
+
+  }
+
+
+  // ========== MANAGE CUSTOMERS ==========
 
   // Get all customers
   getAllCustomers = (req, res) => {
@@ -141,6 +307,27 @@ class AdminController {
       .then(customers => {
         return res.status(200).send(customers)
       })
+  }
+
+  // Get customer info by username
+  getCustomerInfo = (req, res) => {
+    // Data from client
+    let username = req.params.customerUsername;
+    Customer.findOne({ username: username }, { password: 0, __v: 0, activeCode: 0, _id: 0 })
+      .then(acc => {
+        if (acc != null) { // Send data to client
+          return res.status(200).send(acc);
+        }
+        else { // Account not found
+          res.status(404).json({
+            Error: "Customer account not found!"
+          });
+        }
+      })
+
+
+
+
   }
 
   // Update customer
@@ -174,7 +361,7 @@ class AdminController {
   softDeleteCustomer = (req, res) => {
     let username = req.body.username; // Data from client
     let currentTime = DateTime.utc().toISO(); // Current time
-    // Data for soft client
+    // Data for soft delete
     let accSoftDelDetail = {
       status: -1,
       deleteAt: currentTime
@@ -229,6 +416,7 @@ class AdminController {
 
 
   }
+
 
 }
 
--- routes/AdminRouter.js
+++ routes/AdminRouter.js
@@ -9,15 +9,27 @@ router.post('/refresh-token-for-admin', authController.resfreshTokenForAdmin)
 
 router.use(AuthMiddleWare.isAdminAuth)
 
+
 // Manage Admin
 router.get('/get-all-admins', AdminController.getAllAdmins)
+router.get('/get-admin-info/:adminUsername', AdminController.getAdminInfo)
 router.post('/add-admin', AdminController.addAdmin)
 router.post('/update-admin', AdminController.updateAdmin)
 router.post('/soft-delete-admin', AdminController.softDeleteAdmin)
 router.delete('/delete-admin', AdminController.deleteAdmin)
 
+// Manage Employee
+router.get('/get-all-employees', AdminController.getAllEmployees)
+router.get('/get-employee-info/:employeeUsername', AdminController.getEmployeeInfo)
+router.post('/add-employee', AdminController.addEmployee)
+router.post('/update-employee', AdminController.updateEmployee)
+router.post('/soft-delete-employee', AdminController.softDeleteEmployee)
+router.delete('/delete-employee', AdminController.deleteEmployee)
+
+
 // Manage Customer
 router.get('/get-all-customers', AdminController.getAllCustomers)
+router.get('/get-customer-info/:customerUsername', AdminController.getCustomerInfo)
 router.post('/update-customer', AdminController.updateCustomer)
 router.post('/soft-delete-customer', AdminController.softDeleteCustomer)
 router.delete('/delete-customer', AdminController.deleteCustomer)
